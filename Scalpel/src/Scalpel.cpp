e2cb9a0d16593ad64355b3cb3a5bb434e02c9b91
valoeghese <valoeghesemods@gmail.com>
2021-12-20 00:51:23 +1300

    GLEW. Also opengl rendering context exists now.

--- Scalpel/src/Scalpel.cpp
+++ Scalpel/src/Scalpel.cpp
@@ -1,5 +1,6 @@
 #include "Scalpel.h"
 #include <iostream>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 namespace scalpel {
@@ -7,8 +8,31 @@ namespace scalpel {
 		if (!glfwInit()) {
 			return -1;
 		}
-		std::cout << "Hello, World!\n";
-		getchar();
+		std::cout << "GLFW Didn't Die!\n";
+
+		GLFWwindow* window = glfwCreateWindow(500, 500, "Scalpel Linking Test", NULL, NULL);
+
+		if (!window) {
+			return -1;
+		}
+
+		std::cout << "Window Created Successfully!\n";
+
+		glfwMakeContextCurrent(window);
+
+		if (glewInit() != GLEW_OK) {
+			return -1;
+		}
+
+		std::cout << "GLEW Didn't DIE!\n";
+		
+		while (!glfwWindowShouldClose(window)) {
+			glClear(GL_COLOR_BUFFER_BIT);
+			glfwSwapBuffers(window);
+			glfwPollEvents();
+		}
+
+		glfwTerminate();
 		return 0;
 	}
 }
--- ScalpelTests/src/Test.cpp
+++ ScalpelTests/src/Test.cpp
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include "Scalpel.h";
-
+ 
 int main() {
 	return scalpel::test();
 }
